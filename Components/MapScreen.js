2269d7173b5d3d19d2778d4db11820015ae004c0
Aliencodifi <edgarandgonzalez@gmail.com>
2021-01-17 23:15:10 -0500

    Fix MapButton

--- Components/MapScreen.js
+++ Components/MapScreen.js
@@ -7,37 +7,48 @@ import {
   Text,
   PermissionsAndroid,
   Button,
-  Image
+  Image,
+  Dimensions
 } from 'react-native';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 import { getLatestLocation } from 'react-native-location';
 //import RNLocation from 'react-native-location';
 
-
+let {width, height} = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.004; //Very high zoom level
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: '100%',
-    justifyContent: 'flex-end',
+    justifyContent: 'flex-start',
     alignItems: 'center',
+
   },
   map: {
-    ...StyleSheet.absoluteFillObject,
+    height: '85%',
+    width: '100%',
+
   },
+  main: {
+    padding: 0,
+    backgroundColor:"white",
+    display: 'flex',
+  },
+  Buttons: {},
 });
 
 export default function MapScreen() {
 const [currentLatitude, setcurrentLatitude] = useState('0');
 const [currentLongitude, setcurrentLongitude] = useState('0');
 const [watchID, setwatchID] = useState(0);
-
+const [Start, setStart] = useState(false);
+const [ButtonColor, setButtonColor] = useState("green");
+const [ButtonTitle, setButtonTitle] = useState("Start")
  async function requestLocationPermission() {
-  console.log('requestLocationPermission');
   try {
     const granted = await PermissionsAndroid.request(
       PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
@@ -47,11 +58,8 @@ const [watchID, setwatchID] = useState(0);
       },
     );
     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log('You can use the location');
-      alert('You can use the location');
       getLocation();
     } else {
-      console.log('location permission denied');
       alert('Location permission denied');
     }
   } catch (err) {
@@ -63,10 +71,8 @@ const [watchID, setwatchID] = useState(0);
     const callLocation =()=>{
       console.log("CallLocation");
       if(Platform.OS ==='ios'){
-        console.log('ios');
         getLocation();
       }else{
-        console.log('android');
        requestLocationPermission();
 
       }
@@ -96,33 +102,57 @@ const clearLocation =()=>{
   console.log('ClearLocation');
   Geolocation.clearWatch(watchID);
 }
+const  HandleStart=()=>{
+  
+
+if (Start===false){
+  callLocation();
+  setButtonColor("red");
+  setButtonTitle("STOP");
+}
+  else{
+    clearLocation();
+    setButtonColor('green');
+    setButtonTitle('START');
+  }
+setStart(!Start);
+}
     
 
     return (
-      <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={styles.map}
-          region={{
-            latitude: parseFloat(currentLatitude),
-            longitude: parseFloat(currentLongitude),
-            latitudeDelta: 0.04,
-            longitudeDelta: 0.05
-          }}>
-          <Marker
-            coordinate={{
+      <View style={styles.main}>
+        <View style={styles.container}>
+          <MapView
+            style={styles.map}
+            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+            style={styles.map}
+            region={{
               latitude: parseFloat(currentLatitude),
               longitude: parseFloat(currentLongitude),
-            }}/>
-          
-          
-        </MapView>
-
-        <Text>Latitude: {parseFloat(currentLatitude)}</Text>
-        <Text>Longitude: {parseFloat(currentLongitude)}</Text>
-        <Button title="Start" onPress={callLocation} />
-        <Button title="Stop" onPress={clearLocation} />
-
+              latitudeDelta: LATITUDE_DELTA,
+              longitudeDelta: LONGITUDE_DELTA,
+            }}>
+            <Marker
+              coordinate={{
+                latitude: parseFloat(currentLatitude),
+                longitude: parseFloat(currentLongitude),
+              }}
+            />
+          </MapView>
+        </View>
+        <View>
+          <Text>{Start.toString()}</Text>
+          <Text>Latitude: {parseFloat(currentLatitude)}</Text>
+          <Text>Longitude: {parseFloat(currentLongitude)}</Text>
+        </View>
+        <View>
+          <Button
+            style={styles.Buttons}
+             title={ButtonTitle}
+            onPress={HandleStart}
+            color={ButtonColor}
+          />
+        </View>
       </View>
     );
   }
